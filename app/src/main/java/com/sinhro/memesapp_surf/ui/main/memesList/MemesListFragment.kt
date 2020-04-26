3cc8fcaa717521ba9d98f748d377075e9415830a
sinhro200 <sinhro-200@mail.ru>
2020-04-26 16:18:25 +0300

    Сделала корректное обновление мемов  * заменил кастомный RefreshListener на SwipeRefreshLayout

--- app/src/main/java/com/sinhro/memesapp_surf/ui/main/memesList/MemesListFragment.kt
+++ app/src/main/java/com/sinhro/memesapp_surf/ui/main/memesList/MemesListFragment.kt
@@ -2,9 +2,9 @@ package com.sinhro.memesapp_surf.ui.main.memesList
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
 import android.view.*
-import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.LinearInterpolator
@@ -14,12 +14,11 @@ import com.sinhro.memesapp_surf.R
 import com.sinhro.memesapp_surf.customDebugger.CustomDebug
 import com.sinhro.memesapp_surf.model.memes.MemesService
 import com.sinhro.memesapp_surf.ui.SnackbarHelper
-import com.sinhro.memesapp_surf.ui.SwipeListener
 
 
 class MemesListFragment : Fragment() {
     companion object {
-        var instance: MemesListFragment? = null
+        private var instance: MemesListFragment? = null
         fun newInstance(): MemesListFragment {
             if (instance == null)
                 instance = MemesListFragment()
@@ -28,14 +27,12 @@ class MemesListFragment : Fragment() {
     }
 
     private lateinit var searchIv: ImageView
-    private lateinit var refreshIv: ImageView
     private lateinit var errorTv: TextView
     private lateinit var memesListRecycleView: RecyclerView
+    private lateinit var swipeRefreshLayout : SwipeRefreshLayout
 
     private val adapter = RecyclerViewAdapter()
     private val memesService = MemesService()
-
-    private lateinit var rotateAnimation: Animation
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,47 +40,38 @@ class MemesListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val body = inflater.inflate(R.layout.fragment_meme_list, container, false)
-        searchIv = body.findViewById(R.id.memes_list_search_iv)
-        memesListRecycleView = body.findViewById(R.id.memes_list_recView)
-        refreshIv = body.findViewById(R.id.refreshIv)
-        errorTv = body.findViewById(R.id.error_tv)
-
+        findViews(body)
         memesListRecycleView.adapter = adapter
-
-        rotateAnimation =
-            AnimationUtils.loadAnimation(context, R.anim.rotate_center)
-        rotateAnimation.interpolator = LinearInterpolator()
-
-        loadMemes()
-
-        val listener = SwipeListener()
-        listener.OnSwipeTouchListener(context, { loadMemes() }, {}, {}, {})
-        memesListRecycleView.setOnTouchListener(listener)
+        swipeRefreshLayout.setOnRefreshListener { refreshMemes() }
+        refreshMemes()
         return body
     }
 
+    private fun findViews(body: View) {
+        searchIv = body.findViewById(R.id.memes_list_search_iv)
+        memesListRecycleView = body.findViewById(R.id.memes_list_recView)
+        errorTv = body.findViewById(R.id.error_tv)
+        swipeRefreshLayout = body.findViewById(R.id.swipe_refresh_layout)
+    }
 
-    private fun loadMemes() {
-        setLoadState(true)
+
+    private fun refreshMemes() {
+        setLoadingState(true)
         memesService.loadMemes(
             {
                 adapter.setMemes(it)
-                setLoadState(false)
+                setLoadingState(false)
             },
             {
-                setLoadState(false)
+                setLoadingState(false)
                 CustomDebug.log(it.message.toString())
                 setErrorState(true)
             }
         )
     }
 
-    private fun setLoadState(isLoading: Boolean) {
-        refreshIv.visibility = if (isLoading) View.VISIBLE else View.INVISIBLE
-        if (isLoading)
-            refreshIv.startAnimation(rotateAnimation)
-        else
-            refreshIv.clearAnimation()
+    private fun setLoadingState(isLoading: Boolean) {
+        swipeRefreshLayout.isRefreshing = isLoading
     }
 
     private fun setErrorState(isError: Boolean) {
